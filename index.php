19ba1946f3e9b5455b1f18b62e997d8e32a354b4
Mandar Patil <97808555+Mandar1762@users.noreply.github.com>
2022-10-10 19:52:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mandar's Portfolio</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+
+
+</head>
+
+<body>
+  <nav class="navbar">
+    <div class="max-width">
+      <div class="logo"><a href="#" style="text-decoration: none;"> M<span>P</span></a></div>
+      <ul class="menu">
+        <li><a href="#home" class="menu-btn">Home</a></li>
+        <li><a href="#about" class="menu-btn">About</a></li>
+        <li><a href="#Skills" class="menu-btn">Skills</a></li>
+        <li><a href="#projects" class="menu-btn">Projects</a></li>
+        <li><a href="#blogs" class="menu-btn">Blog</a></li>
+        <li><a href="#contact" class="menu-btn">Contact</a></li>
+      </ul>
+      <div class="menu-btn">
+        <i class="fas fa-bars"></i>
+      </div>
+    </div>
+  </nav>
+
+
+  <!-- home section start -->
+  <section class="main1" id="about">
+    <div class="max-width" style="margin-top: 6%;">
+      <div class="main1-content">
+        <div class="column left">
+          <img src="images/Profile.jpg" class="hello" alt="">
+        </div>
+        <div class="column right">
+          <div class="text-1" style="text-align:center">Hello, I'M <span style="color: crimson;">Mandar Patil</span>
+            <div class="text-3">Web Designer | Developer</span></div>
+          </div><br>
+          <div class="text-4" style="padding-bottom: 20px;">Hii I'm B.Tech. Student
+            looking to working in a growth-orientated organization to enhance my technical skills
+            .</div>
+          <br>
+          <ul class="iconsocial">
+            <li><a href="https://www.facebook.com/profile.php?id=100027927458401" target="_blank"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+            <li><a href="https://twitter.com/MandarP64476203" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+            <li><a href="https://github.com/Mandar1762" target="_blank"><i class="fab fa-github" aria-hidden="true"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/mandar-patil-a10131214/" target="_blank"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li>
+            <li><a href="https://www.instagram.com/_mandar__patil/" target="_blank"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <hr>
+
+
+  <section class="about" id="about">
+    <div class="max-width">
+      <h2 class="title">About Me</h2>
+      <div class="about-content">
+        <div class="column left">
+          <div class="text">Hi I'm Mandar</div>
+          <p>I'm full stack web developer living in Jalgaon, Maharashtra. I've completed Diploma in E &
+            TC Engineering from Government Polytechnic, Jalgaon, Maharashtra and pursuing B.Tech in E & TC at
+            Vishwakarma Institute of Technology, Pune.</p>
+          <a href="_Resume.pdf " target="_blank"><button class="bn632-hover bn28">Download CV</button></a>
+        </div>
+        <div class="column right">
+
+          <div class="bars">
+            <div class="text">Languages Known</div>
+            <div class="info">
+              <span>Marathi</span>
+              <span>95%</span>
+            </div>
+            <div class="line css"></div>
+          </div>
+          <div class="bars">
+            <div class="info">
+              <span>Hindi</span>
+              <span>80%</span>
+            </div>
+            <div class="line js"></div>
+          </div>
+          <div class="bars">
+            <div class="info">
+              <span>English</span>
+              <span>75%</span>
+            </div>
+            <div class="line php"></div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <h2 class="title3"><i style="color: crimson;" class="fas fa-graduation-cap"></i> My Education</h2>
+
+  <div class="container" style="margin-left: px;">
+    <div class="timeline">
+      <ul>
+        <li>
+          <span>
+            <h3>Present</h3>
+          </span>
+          <div class="content">
+            <h3 style="margin-bottom: 20px;">Batchelor's Degree</h3>
+            <div class="year">2021-Present</div>
+            <p>
+              Vishwakarma Institute of Technology, Pune
+            </p>
+            <p>Sophomore at VIT With 8.3 CGPA in Current Semester
+            </p>
+          </div>
+        </li>
+        <li>
+          <span>
+            <h3>2021</h3>
+          </span>
+          <div class="content">
+            <h3 style="margin-bottom: 5px;">Diploma Degree</h3>
+            <div class="year">2018-21</div>
+            <p>Government Polytechnic Jalgaon</p>
+            <p>Completed my Diploma Degree With 95.35% Aggregate in Final Year
+            </p>
+          </div>
+        </li>
+        <li>
+          <span>
+            <h3>2018</h3>
+          </span>
+          <div class="content">
+            <h3 style="margin-bottom: 20px;">Intermediate Passout</h3>
+            <div class="year">2014-18
+              <p>La Na Sarvajanik Vidyalay, Jalgaon</p>
+              <p>Completed my High School Education With 84% with A Grade
+              </p>
+            </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <hr>
+  </center>
+  <section class="Skills" id="Skills">
+    <div class="max-width">
+      <h2 class="title"><i style="color: crimson;" class="fas fa-medal"></i> My Skills</h2>
+      <div class="Skill-content">
+        <center>
+          <div class="card">
+            <h1>Languages <i class="fas fa-code fa-spin" style="color: crimson;"></i></h1>
+            <div class="social2">
+              <img src="images/html.png" alt="" class="html">
+              <img src="images/css.png" alt="" class="html" ">
+              <img src=" images/js.png" alt="" class="html" style="max-width: 53px;">
+              <img src="images/php.png" alt="" class="html" style="max-width: 53px;">
+              <img src="images/c.png" alt="" class="html">
+              <img src="images/java.png" alt="" class="html" style="max-width: 53px">
+              <img src="images/xml.png" alt="" class="html">
+              <img src="images/python.png" alt="" class="html">
+              <img src="images/dart.png" alt="" class="html">
+            </div>
+            <br>
+            <br>
+            <h1>Tools & IDE's <i class="fas fa-tools fa-spin" style="margin-left: 5px; color: crimson;"></i></h1>
+            <div class="social2">
+              <img src="images/Github.png" alt="" class="html">
+              <img src="images/VS.png" alt="" class="html" style="max-width: 50px;">
+              <img src="images/Pycharm.png" alt="" class="html" style="max-width: 53px;">
+              <img src="images/android-studio.png" alt="" class="html" style="max-width: 60px;">
+              <img src="images/intellij.png" alt="" class="html">
+              <img src="images/eclipse.png" alt="" class="html" style="max-width: 53px">
+              <img src="images/jupyter.png" alt="" class="html">
+
+            </div>
+            <h1>Frameworks <i class="fas fa-project-diagram fa-spin" style="margin-top: 35px; margin-left: 5px; color: crimson;"></i></h1>
+            <div class="social2">
+              <img src="images/android.png" alt="" style="margin-left: 20px;" class="html">
+              <img src="images/django.png" alt="" class="html" style="max-width: 50px;">
+              <img src="images/bootstrap-logo.png" alt="" class="html">
+              <img src="images/flutter.png" alt="" class="html">
+
+              <br>
+              <br>
+            </div>
+            <h1>Databases <i class="fas fa-database fa-spin" style="margin-top: 35px; margin-left: 5px; color: crimson;"></i></h1>
+            <div class="social3">
+              <img src="images/mysql.png" alt="" class="html" style="max-width: 50px;">
+              <img src="images/sqlite.png" alt="" class="html" style="max-width: 50px;">
+              <img src="images/firebase.png" alt="" class="html">
+              <br>
+              <br>
+            </div>
+            <br>
+            <br>
+          </div>
+      </div>
+      </center>
+    </div>
+    </div>
+  </section>
+  <hr>
+
+  <section class="projects" id="projects">
+    <div class="max-width">
+      <h2 class="title"><i class="fas fa-project-diagram" style="color: crimson;"></i> My Projects</h2>
+      <div class="carousel owl-carousel">
+        <div class="card">
+          <div class="box">
+            <img src="images/onlineexam.png" alt="" class="img12" target="_blank">
+            <div class="text" style="font-size: 22px;">Online Examination System</div>
+            <p>A web application that manages the exams online and secure using django framework</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="box">
+            <img src="images/CARPRICE.png" alt="" class="img12" target="_blank">
+            <div class="text" style="font-size: 22px;">Old Car Price Prediction</div>
+            <p>The aim of project which can effectively identify the cost of the car based on their given parameters
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="box">
+            <img src="images/SHOP.png" alt="" class="img12" target="_blank">
+            <div class="text" style="font-size: 22px;">E-commerce shopping website</div>
+            <p>An e-commerce website is one that allows people to buy and sell physical goods, services, and digital
+              products.</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="box">
+            <img src="images/ICODER.png" alt="" class="img12" target="_blank">
+            <div class="text" style="font-size: 22px;">Blog Writing system </div>
+            <p>the web application that manages the blogs and share our opinion with everyone</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="box">
+            <img src="images/PAYROLL.png" alt="" class="img12" target="_blank">
+            <div class="text" style="font-size: 22px;">Payroll Management System</div>
+            <p>the system that manages the catalog, salary, attendance and another features of Employee </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="box">
+            <img src="images/edI.png" style="height: 150PX;" alt="" class="img12" target="_blank">
+            <div class="text" style="font-size: 20px;">Real-time Automated Face Recognition System for Online Exam
+              Examinee Verification </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+  </section>
+  <section class="blogs" id="blogs">
+    <h2 class="title"><i class="fas fa-podcast" style="color: crimson;"></i> My Posts</h2>
+    <div class="blog-container">
+      <div class="blog-box">
+        <div class="blog-img">
+          <img src="images/DAMBLOG.png" alt="">
+          <div class="blog-text">
+            <a href="https://damsiniot.blogspot.com/2022/05/useof-iot-in-construction-of-dams.html" style="color: #0D6EFD;" target="_blank">
+              <b>Use of IoT in construction of Dams</b></a>
+            <p> For proper management of water, there is a need to develop a proper monitoring system for the opening
+              gate of dams to maintain the proper water level in dams. We can use IOT for improving the dam monitoring
+              system. <a href="https://damsiniot.blogspot.com/2022/05/useof-iot-in-construction-of-dams.html" style="color: #0D6EFD;" class="" target="_blank">Read more</a></p>
+            </p>
+            <span style="margin-top: 30px;"><b>Published On -</b> May 28, 2022, 2:46 p.m.</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="blog-box">
+        <div class="blog-img">
+          <img src="images/IPV6.jpg" alt="">
+          <div class="blog-text">
+            <a href="https://needforipv6.blogspot.com/2022/02/ha-dc.html" style="color: #0D6EFD;" target="_blank">
+              <b> Need of IPv6</b></a>
+            <p> Internet Protocol Version 6 (IPv6) is a network layer protocol that enables data communications over a
+              packet switched network. IPv6 The main function of the IPv6 is to create the more unique IP address
+              identifiers <a href="https://needforipv6.blogspot.com/2022/02/ha-dc.html" style="color: #0D6EFD;" class="" target="_blank">Read more</a></p>
+            </p>
+            <span style="margin-top: 30px;"><b>Published On -</b> Feb 17, 2022, 5:30 p.m.</span>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
+  <hr>
+  <section class="contact" id="contact">
+    <div class="max-width">
+      <h2 class="title"><i class="fas fa-phone-alt" style="color: crimson;"></i> Contact Me</h2>
+      <div class="contact-content">
+        <div class="column left">
+          <div class="text">
+
+            <div class="icons">
+              <div class="row">
+                <i class="fas fa-user"></i>
+                <div class="info">
+                  <div class="head">Name :</div>
+                  <div class="sub-title">Mandar Patil</div>
+                </div>
+              </div>
+              <br>
+              <div class="icons">
+                <div class="row">
+                  <i class="fas fa-phone-alt"></i>
+                  <div class="info">
+                    <div class="head">Mobile no :</div>
+                    <div class="sub-title">9284587732</div>
+                  </div>
+                </div>
+                <br>
+                <div class="row">
+                  <i class="fas fa-map-marker-alt"></i>
+                  <div class="info">
+                    <div class="head">Address :</div>
+                    <div class="sub-title">bibwewadi, pune</div>
+                  </div>
+                </div>
+                <br>
+                <div class="row">
+                  <i class="fas fa-envelope"></i>
+                  <div class="info">
+                    <div class="head">Email :</div>
+                    <div class="sub-title">mandarpatil1762@gmail.com</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br>
+          <div class="column right">
+            <form id="contact-form" method="post" role="form">
+              <div class="fields">
+                <div class="field name">
+                  <input type="text" id="name" name="name" placeholder="Name">
+                </div>
+                <div class="field email">
+                  <input type="email" id="email" name="email" placeholder="Email">
+                </div>
+              </div>
+              <div class="fields">
+                <div class="field name">
+                  <input type="text" id="contact" name="contact" placeholder="contact">
+                </div>
+                <div class="field email">
+                  <input type="text" id="subject" name="subject" placeholder="subject">
+                </div>
+              </div>
+              <div class="field-textarea">
+                <textarea name="text" id="text" cols="30" rows="10" placeholder="Enter message"></textarea>
+              </div>
+              <br>
+              <button type="submit" name="ok" class="bn632-hover bn28">Submit</button></a>
+            </form>
+            <?php
+            if (isset($_POST['ok'])) {
+              include_once 'function.php';
+              $obj = new Contact();
+              $res = $obj->contact_us($_POST);
+              if ($res == false) {
+                echo "<script>alert('Query successfully Submitted.Thank you')</script>";
+              } else {
+                echo "<script>alert('Something Went wrong!!')</script>";
+              }
+            }
+            ?>
+
+
+
+          </div>
+        </div>
+      </div>
+  </section>
+
+  <footer>
+    <div class="box">
+      <span>Site by <a href="#" style="text-decoration: none;">Mandar Patil <i class="fas fa-heart"></i></a></span>
+    </div>
+  </footer>
+
+
+  <script src="script.js"></script>
+</body>
+
+</html>
